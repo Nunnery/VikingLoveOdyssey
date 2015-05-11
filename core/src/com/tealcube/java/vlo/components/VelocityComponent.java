e1b8c908b541b93aead2d623223861db77fd829a
Richard Harrah <topplethenunnery@gmail.com>
2015-05-11 17:43:01 -0400

    adding VelocityComponent

---
+++ core/src/com/tealcube/java/vlo/components/VelocityComponent.java
@@ -0,0 +1,25 @@
+package com.tealcube.java.vlo.components;
+
+public class VelocityComponent {
+
+    private int x = 0, y = 0;
+
+    public int getX() {
+        return x;
+    }
+
+    public VelocityComponent setX(int x) {
+        this.x = x;
+        return this;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public VelocityComponent setY(int y) {
+        this.y = y;
+        return this;
+    }
+
+}
